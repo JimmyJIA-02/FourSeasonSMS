6e2e04cd4f23a6cdb5c051b9b52b12e8488c3b9a
JimmyJIA-02 <jiamorning3@gmail.com>
2025-07-10 21:26:14 +1000

    java project initialisation

---
+++ FourSeasonSMS/src/DataReader.java
@@ -0,0 +1,7 @@
+public class DataReader {
+    // read data from the file
+    public static String readPhoneNumber(){
+        // read the first column of the file
+        return "1234567890";
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # FourSeasonSMS
 An sms sender
+
+## Requirements
+
+- 选中行，给定行数区间，发送特定区间的对应内容
+
+- 特定内容，给定文字，发送特定内容的信息，判断x column是否为空，如果非空直接跳过
+
+- 
